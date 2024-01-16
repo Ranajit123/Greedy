2281eef1a8df1f589c0779b4af4d51f2b5f13e3e
Ranajit Das <das143ranajit@gmail.com>
2024-01-16 10:08:55 +0530

    add indian coins question

--- IndiansCoins.java
+++ IndiansCoins.java
@@ -1,3 +1,19 @@
+/*
+  we are given an infinite supply of denominations [1, 2, 5, 10, 20, 50 , 100, 500, 1000] find min no. of coins/note to change the value v
+
+  sample input ->
+  v=590
+
+  sample output ->
+  ans = 4
+
+  Pseudo Code ->
+
+  step1 -> sort the array in descending order
+  step2 -> take a variable for counting the total number of coins
+  step3 -> traverse through the array and check the array element is less than the value or not, if less then increase the coin counter and decrease the value 
+  step 4 -> return the coin counter
+ */
 import java.util.*;
 public class IndiansCoins {
   public static int mimCoinReq(Integer arr[], int price){
